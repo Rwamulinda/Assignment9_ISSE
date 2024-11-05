8667eda52f96898701262d6a09cdade142bf48cd
uwasepauline7 <puwase@andrew.cmu.edu>
2024-11-05 19:20:24 +0200

    testing

--- ew_test.c
+++ ew_test.c
@@ -303,9 +303,9 @@ int test_tokenize_input()
   //
   // TODO: Add your code here
 
-  test_assert(TOK_tokenize_input("2 + + 3", errmsg, sizeof(errmsg)) == NULL);
+  test_assert(TOK_tokenize_input("2+(3", errmsg, sizeof(errmsg)) == NULL);
   test_assert(strlen(errmsg) != 0);
-  test_assert(strcasecmp(errmsg, "Unexpected token PLUS") == 0);
+  test_assert(strcasecmp(errmsg, "Position 3: unexpected token CLOSE_PAREN") == 0);
 
   test_assert(TOK_tokenize_input("3 sine", errmsg, sizeof(errmsg)) == NULL);
   test_assert(strlen(errmsg) != 0);
